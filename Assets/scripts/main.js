dfcc691f3a691e4f27d0c6b75c43c09b084237bf
Tian Lan <welkinlan@gmail.com>
2015-06-10 12:13:18 -0500

    initial commit
    two scenes
    images

---
+++ Assets/scripts/main.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+var choice1_img : GUITexture;
+var choice2_img : GUITexture;
+var choice1_txt : GUIText;
+var choice2_txt : GUIText;
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnGUI(){
+	
+}
---
+++ Assets/scripts/menu.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+var skin : GUISkin;
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnGUI() {
+
+	GUI.skin = skin;
+	
+	if (GUI.Button(HelpClass.ScrRectCenter2(0.5,0.6,0.3,0.075),"Start")) {
+		Application.LoadLevel(1);
+	}
+	
+}
